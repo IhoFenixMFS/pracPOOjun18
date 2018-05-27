26d4ff4787f66bb2a79af6424b8a73c2792bfe95
IhoFenixMFS <iho-ladamadeltiempo@live.com>
2018-05-27 22:11:53 +0200

    Incluyendo clases

--- src/General/InterfazDeUsuario.java
+++ src/General/InterfazDeUsuario.java
@@ -2,6 +2,36 @@ package General;
 
 public class InterfazDeUsuario {
 
+	public static void menu() {
+		System.out.println();
+		System.out.println();
+		System.out.println("0.- Salir");
+		System.out.println("------------------ ESTADO --------------");
+		System.out.println("1.- Imprimir estado de los clientes");
+		System.out.println("2.- Imprimir estado de la bolsa");
+		System.out.println("------------------- BANCO --------------");
+		System.out.println("3.- Añadir cliente");
+		System.out.println("4.- Eliminar cliente");
+		System.out.println("5.- Realizar copia de seguridad");
+		System.out.println("6.- Restaurar copia de seguridad");
+		System.out.println("7.- Mejorar cliente a premium");
+		System.out.println("8.- Solicita recomendación de inversión");
+		System.out.println("------------------- BOLSA --------------");
+		System.out.println("9.- Añadir empresa a la bolsa");
+		System.out.println("10.- Eliminar empresa de la bolsa");
+		System.out.println("11.- Actualización de valores");
+		System.out.println("12.- Realizar copia de seguridad");
+		System.out.println("13.- Restaurar copia de seguridad");
+		System.out.println("------------- OPERACIONES --------------");
+		System.out.println("14.- Solicitar compra de acciones");
+		System.out.println("15.- Solicitar venta de acciones");
+		System.out.println("16.- Solicitar actualización de valores");
+		System.out.println("------------------- BRÓKER --------------");
+		System.out.println("17.- Imprimir operaciones pendientes");
+		System.out.println("18.- Ejecutar operaciones pendientes");
+		System.out.println("-----------------------------------------");
+	}
+
 	@SuppressWarnings("unused")
 	public static void seleccion(Integer opcion) {
 		switch (opcion) {
--- src/General/Utilidades.java
+++ src/General/Utilidades.java
@@ -1,5 +1,15 @@
 package General;
 
 public class Utilidades {
-
+	
+	/*Generamos un número aleatorio decimal*/
+	private double numAleatt(Integer dsd, Integer hst) {
+		return (Math.random()*(hst-1)+dsd);
+	}
+	/*Generamos un número aleatorio entero*/
+	public int numAleatInt (Integer dsd, Integer hst) {
+		return (int)(numAleatInt(dsd, hst));
+	}
+	
+	
 }
