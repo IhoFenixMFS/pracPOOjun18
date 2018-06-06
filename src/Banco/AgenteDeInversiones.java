d350c9b7f5ee01e6b2b133de9ff5b3b8b23706ee
Marina F. S <32669834+IhoFenixMFS@users.noreply.github.com>
2018-06-07 00:27:54 +0200

    rectificando 2

--- src/Banco/AgenteDeInversiones.java
+++ src/Banco/AgenteDeInversiones.java
@@ -38,12 +38,12 @@ public class AgenteDeInversiones extends Persona{
 	}
 
 	public void ComprarAccion(){
-		System.out.println("Comprar acción. . .");
+		System.out.println("Comprar acciÃ³n. . .");
 		//compraAcciones();
 	}
 	
 	public void VenderAccion(){
-		System.out.println("Vender acción. . .");
+		System.out.println("Vender acciÃ³n. . .");
 		//venderAcciones();
 	}
 
@@ -89,7 +89,11 @@ public class AgenteDeInversiones extends Persona{
 		double invertido = numAcciones * empresa.getValorAcciones();
 		//calculamos el dinero que le sobra.
 		double devolver = importe - invertido;
-
+		
+		//buscar entre todos los clientes del banco el elegido.	+
+-		Cliente cli = Banco.buscarPorNombre(nomCli);	
+-		Cliente.actualizarValoresCliente(cli, invertido, numAcciones, nomEmp);
+		
 		MensajeRespuestaCompra.mensajeCompraAcciones(id, nomCli, nomEmp, importe, numAcciones, invertido, devolver);
 	}
 }
